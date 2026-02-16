7177c000e3c814905a23d65787462299957a8b17
richiramirez0304 <ricardo.silva@uniminuto.edu.co>
2026-02-16 11:55:45 -0500

    Mas Arreglos

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,8 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    "@tailwindcss/postcss": {}, // <--- ESTA ES LA CLAVE (lleva arroba)
+    autoprefixer: {},
   },
 };
 
